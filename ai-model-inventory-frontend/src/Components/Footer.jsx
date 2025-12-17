beed2d7aee6f828d73913220a4e051cd46417aec
mehrab-abir <mehrababir100@gmail.com>
2025-12-17 03:45:37 -0500

    Responsive navbar

--- ai-model-inventory-frontend/src/Components/Footer.jsx
+++ ai-model-inventory-frontend/src/Components/Footer.jsx
@@ -8,27 +8,27 @@ import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
     return (
-      <footer className="bg-[#0d0026] py-10 text-white">
+      <footer className="bg-surface pt-15 pb-6 text-secondary">
         <div className="w-10/12 mx-auto flex flex-col md:flex-row justify-between mb-10">
           <div className="flex-2">
             <div className="flex items-center">
               <img src={logo} alt="" className="w-18" />
               <h3 className="text-lg font-semibold">AI Model Inventory</h3>
             </div>
-            <p className="w-full md:w-1/2">
+            <p className="w-full md:w-4/5">
               Manage and discover AI models with ease. A comprehensive platform
               for AI enthusiasts and developers.
             </p>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 mt-10 md:mt-0">
             <h4 className="text-lg font-semibold">Quick Links</h4>
-            <div className="flex flex-col items-start mt-5 space-y-3">
+            <div className="flex flex-col items-start mt-5 space-y-2.5">
               <Link>Home</Link>
               <Link>All Models</Link>
               <Link>Add Model</Link>
             </div>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 mt-10 md:mt-0">
             <h4 className="text-lg font-semibold">Connect</h4>
             <div className="flex items-center mt-5 gap-8">
               <FaSquareFacebook className="text-2xl cursor-pointer" />
@@ -39,7 +39,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t border-primary opacity-20"></div>
-        <div className='text-center space-y-3 mt-10 text-muted'>
+        <div className="text-center space-y-3 mt-5 text-muted">
           <p>&copy; Copyright Reserved - 2025</p>
           <p>-Mehrab Jalil Abir-</p>
         </div>
--- ai-model-inventory-frontend/src/Components/Header.jsx
+++ ai-model-inventory-frontend/src/Components/Header.jsx
@@ -1,48 +1,85 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import logo from "./../assets/logo-transparent.png";
-import { NavLink } from 'react-router';
+import { Link, NavLink } from "react-router";
 import { CiLight } from "react-icons/ci";
 import { MdDarkMode } from "react-icons/md";
+import { FaRegTimesCircle } from "react-icons/fa";
 
 const Header = () => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
 
-    const [theme, setTheme] = useState(()=>localStorage.getItem("theme") || "light");
+  const [openMenu, setOpenMenu] = useState(false);
 
-    useEffect(()=>{
-        const html = document.querySelector('html');
-        html.setAttribute("data-theme",theme);
-        localStorage.setItem("theme",theme);
-    },[theme])
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
-
-    return (
-      <header className="bg-surface fixed w-full z-50">
-        <div className="w-10/12 mx-auto flex items-center justify-between">
-          <div className="logo flex gap-1 items-center">
+  return (
+    <header className="bg-surface fixed w-full z-50">
+      <div className="w-10/12 mx-auto flex items-center justify-between">
+        <div className="logo flex gap-1 items-center">
+          <div
+            onClick={() => setOpenMenu(!openMenu)}
+            className="flex flex-col md:hidden space-y-1 cursor-pointer"
+          >
+            <span className="h-1 bg-gray-400 rounded-md w-7"></span>
+            <span className="h-1 bg-gray-400 rounded-md w-5"></span>
+            <span className="h-1 bg-gray-400 rounded-md w-7"></span>
+          </div>
+          <Link to='/' className="flex items-center">
             <img src={logo} alt="" className="w-20" />
             <h1 className="text-xl font-bold">AI Models</h1>
-          </div>
-
-          <nav className="flex items-center gap-6">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/">All Models</NavLink>
-            <NavLink to="/">Add a Model</NavLink>
-          </nav>
-
-          <div className="user-action flex items-center space-x-2">
-            {
-                theme === "light" ? <MdDarkMode onClick={()=>setTheme("dark")} className="text-4xl cursor-pointer" /> : <CiLight onClick={()=>setTheme("light")} className="text-4xl cursor-pointer" />
-            }
-            
-            
-            <button className="btn bg-surface border border-primary">
-              Login
-            </button>
-            <button className="btn bg-primary text-white">Register</button>
-          </div>
+          </Link>
         </div>
-      </header>
-    );
+
+        <nav className="hidden md:flex items-center gap-6 ">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">All Models</NavLink>
+          <NavLink to="/">Add a Model</NavLink>
+        </nav>
+
+        <div className="user-action flex items-center space-x-2">
+          {theme === "light" ? (
+            <MdDarkMode
+              onClick={() => setTheme("dark")}
+              className="text-2xl md:text-4xl cursor-pointer"
+            />
+          ) : (
+            <CiLight
+              onClick={() => setTheme("light")}
+              className="text-2xl md:text-4xl cursor-pointer"
+            />
+          )}
+
+          <button className="btn bg-surface border border-primary">
+            Login
+          </button>
+          <button className="btn bg-primary text-white">Register</button>
+        </div>
+      </div>
+
+      {/* responsive navbar-small device */}
+      <div
+        className={`fixed w-full h-full top-0 right-0 bg-surface text-secondary ${
+          openMenu ? "" : "-translate-x-full"
+        } transition-all duration-500`}
+      >
+        <FaRegTimesCircle
+          onClick={() => setOpenMenu(!openMenu)}
+          className="text-4xl absolute top-10 right-10 cursor-pointer text-gray-400"
+        />
+        <nav className="flex flex-col items-center mt-20 space-y-2.5">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">All Models</NavLink>
+          <NavLink to="/">Add a Model</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
